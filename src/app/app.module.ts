0a821e1abf29289b0abb4f87e45eefacdb7c4e17
ningge <weiguangningtian@gmail.com>
2017-11-27 16:08:02 +0800

    init framework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { AppRoutes } from './app.route';
+
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FixedPluginComponent } from './shared/fixedplugin/fixedplugin.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FooterComponent,
+    NavbarComponent,
+    FixedPluginComponent,
+    SidebarComponent,
+    FixedPluginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(AppRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+
+declare var $: any;
+
+@Component({
+  selector: 'dashboard-cmp',
+  moduleId: module.id,
+  templateUrl: 'dashboard.component.html'
+})
+
+export class DashboardComponent implements OnInit {
+  ngOnInit() {
+  }
+}
